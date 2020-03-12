25159589eb301166e92c485fed0f59c5f1d35164
navidhsz <navid.hsz@gmail.com>
2020-03-12 17:08:38 +0000

    add exercise 1,2,3 of chapter 7

---
+++ lib/programming_elixir/chapter7.ex
@@ -0,0 +1,34 @@
+defmodule ProgrammingElixir.Chapter7 do
+  @moduledoc false
+
+  # Exercise listAndRecursion-1
+
+  def mapsum([],_func), do: 0
+
+  def mapsum([head | tail],func) do
+       func.(head) + mapsum(tail,func)
+  end
+
+  # Exercise listAndRecursion-2
+
+  def max(list) do
+    [head | list] = list
+    maxHelper(list,head)
+  end
+
+  defp maxHelper([],currentValue), do: currentValue
+  defp maxHelper([head | tail], currentValue) when head != nil and head >= currentValue , do: maxHelper(tail,head)
+  defp maxHelper([head | tail], currentValue) when head != nil and head < currentValue , do: maxHelper(tail,currentValue)
+
+
+  # Exercise listAndRecursion-3
+
+  def caesar(list,n) do
+    caesarHelper(list,n)
+  end
+
+  defp caesarHelper([],n), do: []
+  defp caesarHelper([head | tail],n) when head+n <= ?z, do: [head+n | caesarHelper(tail,n)]
+  defp caesarHelper([head | tail],n) when head+n > ?z, do: [head+n-?z+?a-1 | caesarHelper(tail,n)]
+
+end
---
+++ test/programming_elixir/chapter7_test.exs
@@ -0,0 +1,29 @@
+defmodule ProgrammingElixir.Chapter7Test do
+  use ExUnit.Case
+
+  alias ProgrammingElixir.Chapter7
+
+  @moduletag :capture_log
+
+  doctest Chapter7
+
+  # Exercise listAndRecursion-1
+
+  test "should return 14 for mapsum([,2,3],&(&1*&1)" do
+    assert Chapter7.mapsum([1,2,3],&(&1*&1)) == 14
+  end
+
+  # Exercise listAndRecursion-2
+
+  test "should return 60 for max([1,2,3,4,20,40,10,5,60])" do
+    assert Chapter7.max([1,2,3,40,-20,40,10,5,60]) == 60
+  end
+
+  # Exercise listAndRecursion-3
+
+  test "should return 'elixir' for caesar('ryvkve',13)" do
+    assert Chapter7.caesar('ryvkve',13) == 'elixir'
+  end
+
+
+end
