a5c9b555b54b6df61b12d31bec706f427ad8c403
nhassanzadeh <nhassanzadeh@groupon.com>
2020-03-18 18:24:34 +0000

    add exercise 1 to 5 of chapter 15

---
+++ lib/programming_elixir/chapter15.ex
@@ -0,0 +1,81 @@
+defmodule ProgrammingElixir.Chapter15 do
+  @moduledoc false
+
+  # Exercise: Working with multiple processes-1
+
+  def counter(next_pid) do
+    receive do
+      n -> send(next_pid, n + 1)
+    end
+  end
+
+  def create_processes(n) do
+    code_to_run = fn _, send_to -> spawn(ProgrammingElixir.Chapter15, :counter, [send_to]) end
+    last = Enum.reduce(1..n, self(), code_to_run)
+    send(last, 0)
+
+    receive do
+      final_answer when is_integer(final_answer) -> "Result is #{inspect(final_answer)}"
+    end
+  end
+
+  def run(n) do
+    :timer.tc(ProgrammingElixir.Chapter15, :create_processes, [n]) |> IO.inspect()
+  end
+
+  # Exercise: Working with multiple processes-2
+  # spawning process is not deterministic, but messages published to the mailbox of a process will be processed in order (deterministic)
+
+  def process(name, token, canAccept) do
+    receive do
+      {sender, message} when token == message ->
+        IO.inspect("received on message=#{message} on process=#{name}")
+        send(sender, message)
+    end
+  end
+
+  def runExercise2() do
+    token1 = :fred
+    token2 = :betty
+    pid1 = spawn(ProgrammingElixir.Chapter15, :process, [:process1, token1, true])
+    send(pid1, {self, token1})
+    # make sure token2 is published after token1. Not an elegant way to handle ordering!
+    :timer.sleep(1000)
+    pid2 = spawn(ProgrammingElixir.Chapter15, :process, [:process2, token2, false])
+    send(pid2, {self, token2})
+  end
+
+  # Exercise: Working with multiple processes-3
+
+  def processExercise3(parent) do
+    send(parent, {self(), :testMsg})
+
+    # with this line commented test will fails because parent process does not have enough time to receive the message
+    :timer.sleep(100)
+    exit(:exit)
+  end
+
+  # Exercise: Working with multiple processes-4
+
+  def processExercise4(parent) do
+    send(parent, {self(), :testMsg})
+
+    # with this line commented test will fails because parent process does not have enough time to receive the message
+    :timer.sleep(100)
+    raise "test exception"
+  end
+
+  # Exercise: Working with multiple processes-5_1
+
+  def processExercise5_1(parent) do
+    send(parent, {self(), :testMsg})
+    exit(:exit)
+  end
+
+  # Exercise: Working with multiple processes-5_2
+
+  def processExercise5_2(parent) do
+    send(parent, {self(), :testMsg})
+    raise "test exception"
+  end
+end
---
+++ test/programming_elixir/chapter15_test.exs
@@ -0,0 +1,113 @@
+defmodule ProgrammingElixir.Chapter15Test do
+  use ExUnit.Case
+
+  alias ProgrammingElixir.Chapter15
+
+  @moduletag :capture_log
+
+  doctest Chapter15
+
+  # Exercise: Working with multiple processes-1
+
+  test "nothing to test - Exec1" do
+    Chapter15.run(1000)
+  end
+
+  # Exercise: Working with multiple processes-2
+
+  test "should receive tokens in order" do
+    Chapter15.runExercise2()
+
+    receive do
+      message -> assert message == :fred
+    end
+
+    receive do
+      message -> assert message == :betty
+    end
+  end
+
+  # Exercise: Working with multiple processes-3
+
+  test "should have valid pid and message - processes-3" do
+    pid = spawn_link(ProgrammingElixir.Chapter15, :processExercise3, [self()])
+
+    # :timer.sleep(500)  # with this line uncommented test will fails because parent process gets shutdown
+    receive do
+      {sender, message} ->
+        IO.inspect("pid=#{inspect(pid)} , sender=#{inspect(sender)} , message=#{message}")
+        assert pid == sender
+        assert message == :testMsg
+    end
+  end
+
+  # Exercise: Working with multiple processes-4
+
+  test "should have valid pid and message - processes-4" do
+    pid = spawn_link(ProgrammingElixir.Chapter15, :processExercise4, [self()])
+
+    # :timer.sleep(500)  # with this line uncommented test will fails because parent process gets shutdown
+    receive do
+      {sender, message} ->
+        IO.inspect("pid=#{inspect(pid)} , sender=#{inspect(sender)} , message=#{message}")
+        assert pid == sender
+        assert message == :testMsg
+    end
+  end
+
+  # Exercise: Working with multiple processes-5_1
+
+  test "should have valid pid and message - processes-5_1" do
+    {pid, ref} = spawn_monitor(ProgrammingElixir.Chapter15, :processExercise5_1, [self()])
+    :timer.sleep(500)
+    # even after delay we receive message from already shutdown child process
+    receive do
+      {sender, message} ->
+        IO.inspect("pid=#{inspect(pid)} , sender=#{inspect(sender)} , message=#{message}")
+        assert pid == sender
+        assert message == :testMsg
+    end
+
+    # At the end, we receive shutdown/error message
+    receive do
+      {:DOWN, child_ref, :process, child_pid, error_msg} ->
+        IO.inspect(
+          "DOWN: pid=#{inspect(pid)} , sender=#{inspect(child_pid)} , message=#{error_msg}"
+        )
+
+        assert pid == child_pid
+        assert ref == child_ref
+        assert error_msg == :exit
+    end
+  end
+
+  # Exercise: Working with multiple processes-5_2
+
+  test "should have valid pid and message - processes-5_2" do
+    {pid, ref} = spawn_monitor(ProgrammingElixir.Chapter15, :processExercise5_2, [self()])
+    :timer.sleep(500)
+    # even after delay we receive message from already shutdown child process
+    receive do
+      {sender, message} ->
+        IO.inspect("pid=#{inspect(pid)} , sender=#{inspect(sender)} , message=#{message}")
+        assert pid == sender
+        assert message == :testMsg
+    end
+
+    # At the end, we receive shutdown/error message
+    receive do
+      {:DOWN, child_ref, :process, child_pid, {exception, stacktrace}} ->
+        exception_msg = Exception.message(exception)
+
+        IO.inspect(
+          "DOWN: pid=#{inspect(pid)} , sender=#{inspect(child_pid)} , exception_message=#{
+            exception_msg
+          }"
+        )
+
+        assert pid == child_pid
+        assert ref == child_ref
+        assert "test exception" == exception_msg
+    end
+  end
+end
