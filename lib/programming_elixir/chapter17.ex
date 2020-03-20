86089fe21c8fa66ad53cfef3f09b8c3846d050cb
navidhsz <nhassanzadeh@groupon.com>
2020-03-20 21:59:54 +0000

    add exercise 3,4,5 of chapter 17

--- lib/programming_elixir/chapter17.ex
+++ lib/programming_elixir/chapter17.ex
@@ -21,17 +21,35 @@ defmodule ProgrammingElixir.Chapter17 do
     end
   end
 
-  # Exercise: OTP-Server-1 , OTP-Server-2
+  # Exercise: OTP-Server-1
+  #           OTP-Server-2
+  #           OTP-Server-3 (the name is :stack and specified in test)
+  #           OTP-Server-4
+  #           OTP-Server-5
 
   defmodule Stack do
     use GenServer
+
+    @stack ProgrammingElixir.Chapter17.Stack
+
+    def create(initial_stack_values, name) do
+      GenServer.start_link(@stack, initial_stack_values, name: name)
+    end
+
+    def pop(name) do
+      GenServer.call(name, :pop)
+    end
+
+    def push(value, name) do
+      GenServer.cast(name, {:push, value})
+    end
 
     def init(initial_stack_values) do
       {:ok, initial_stack_values}
     end
 
     def handle_call(:pop, _from, []) do
-      Process.exit(self(), :normal)
+      raise "cannot pop from empty stack"
     end
 
     def handle_call(:pop, _from, initial_stack_values) do
@@ -45,6 +63,11 @@ defmodule ProgrammingElixir.Chapter17 do
 
     def handle_cast({:push, new_value}, initial_stack_values) do
       {:noreply, [new_value | initial_stack_values]}
+    end
+
+    def terminate(reason, state) do
+      IO.puts(inspect(reason))
+      Process.exit(self(), :normal)
     end
   end
 end
--- test/programming_elixir/chapter17_test.exs
+++ test/programming_elixir/chapter17_test.exs
@@ -7,22 +7,40 @@ defmodule ProgrammingElixir.Chapter17Test do
 
   doctest Chapter17
 
+  # Exercise: OTP-Server-1 , OTP-Server-2 , OTP-Server-3
+
   test "pop operation of stack" do
-    {:ok, pid} = GenServer.start_link(ProgrammingElixir.Chapter17.Stack, [5, "cat", 9])
-    assert GenServer.call(pid, :pop) == 5
-    assert GenServer.call(pid, :pop) == "cat"
-    assert GenServer.call(pid, :pop) == 9
-    assert catch_exit(GenServer.call(pid, :pop))
+    {:ok, _pid} =
+      GenServer.start_link(ProgrammingElixir.Chapter17.Stack, [5, "cat", 9], name: :stack)
+
+    assert GenServer.call(:stack, :pop) == 5
+    assert GenServer.call(:stack, :pop) == "cat"
+    assert GenServer.call(:stack, :pop) == 9
+    assert catch_exit(GenServer.call(:stack, :pop))
   end
 
   test "push operation of stack" do
-    {:ok, pid} = GenServer.start_link(ProgrammingElixir.Chapter17.Stack, [])
-    GenServer.cast(pid, {:push, 9})
-    GenServer.cast(pid, {:push, "cat"})
-    GenServer.cast(pid, {:push, 5})
+    {:ok, _pid} = GenServer.start_link(ProgrammingElixir.Chapter17.Stack, [], name: :stack)
+    GenServer.cast(:stack, {:push, 9})
+    GenServer.cast(:stack, {:push, "cat"})
+    GenServer.cast(:stack, {:push, 5})
 
-    assert GenServer.call(pid, :pop) == 5
-    assert GenServer.call(pid, :pop) == "cat"
-    assert GenServer.call(pid, :pop) == 9
+    assert GenServer.call(:stack, :pop) == 5
+    assert GenServer.call(:stack, :pop) == "cat"
+    assert GenServer.call(:stack, :pop) == 9
+  end
+
+  # Exercise: OTP-Server-4
+
+  test "push/pop operation using API" do
+    stack_name = :my_stack
+    Chapter17.Stack.create([], stack_name)
+    Chapter17.Stack.push(9, stack_name)
+    Chapter17.Stack.push("cat", stack_name)
+    Chapter17.Stack.push(5, stack_name)
+
+    assert Chapter17.Stack.pop(stack_name) == 5
+    assert Chapter17.Stack.pop(stack_name) == "cat"
+    assert Chapter17.Stack.pop(stack_name) == 9
   end
 end
