6523071cc05d5e0a024c505220344a50729989c4
navidhsz <nhassanzadeh@groupon.com>
2020-03-23 22:42:21 +0000

    add exercise 2 of chapter 18

--- lib/programming_elixir/chapter18.ex
+++ lib/programming_elixir/chapter18.ex
@@ -36,6 +36,11 @@ defmodule ProgrammingElixir.Chapter18 do
     @impl true
     def handle_call(:pop, _from, initial_stack_values) do
       [head | tail] = initial_stack_values
+
+      if head == "kill" do
+        raise "kill stack process"
+      end
+
       {:reply, head, tail}
     end
 
@@ -46,6 +51,107 @@ defmodule ProgrammingElixir.Chapter18 do
 
     @impl true
     def handle_cast({:push, new_value}, initial_stack_values) do
+      {:noreply, [new_value | initial_stack_values]}
+    end
+
+    @impl true
+    def terminate(reason, state) do
+      Process.exit(self(), :normal)
+    end
+  end
+
+  # -------------------------
+  # Exercise: OTP-Supervisors-1
+
+  defmodule StackAppWithStash do
+    def start(args) do
+      children = [
+        {ProgrammingElixir.Chapter18.Stash, []},
+        {ProgrammingElixir.Chapter18.StackWithStash, args}
+      ]
+
+      opts = [strategy: :one_for_one, name: __MODULE__]
+      Supervisor.start_link(children, opts)
+    end
+  end
+
+  defmodule Stash do
+    use GenServer
+
+    @stash __MODULE__
+
+    @impl true
+    # ignore the state passed from client and use from stash
+    def init(init_state) do
+      {:ok, init_state}
+    end
+
+    def start_link(state) do
+      GenServer.start_link(__MODULE__, state, name: __MODULE__)
+    end
+
+    def get() do
+      GenServer.call(@stash, {:get})
+    end
+
+    def update(new_state) do
+      GenServer.cast(@stash, {:update, new_state})
+    end
+
+    @impl true
+    def handle_call({:get}, _from, initial_stack_values) do
+      {:reply, initial_stack_values, initial_stack_values}
+    end
+
+    @impl true
+    def handle_cast({:update, new_state}, _old_state) do
+      {:noreply, new_state}
+    end
+  end
+
+  defmodule StackWithStash do
+    use GenServer
+
+    def start_link(state) do
+      GenServer.start_link(__MODULE__, state, name: __MODULE__)
+    end
+
+    # callbacks
+
+    @impl true
+    # ignore the state passed from client and use from stash
+    def init(_) do
+      {:ok, ProgrammingElixir.Chapter18.Stash.get()}
+    end
+
+    @impl true
+    def handle_call(:pop, _from, []) do
+      raise "cannot pop from empty stack"
+    end
+
+    @impl true
+    def handle_call(:pop, _from, initial_stack_values) do
+      [head | tail] = initial_stack_values
+
+      if head == "kill" do
+        ProgrammingElixir.Chapter18.Stash.update(tail)
+        # emulate process crash
+        raise "kill stack process"
+      end
+
+      ProgrammingElixir.Chapter18.Stash.update(tail)
+      {:reply, head, tail}
+    end
+
+    @impl true
+    def handle_cast({:push, []}, initial_stack_values) do
+      ProgrammingElixir.Chapter18.Stash.update([initial_stack_values])
+      {:noreply, [initial_stack_values]}
+    end
+
+    @impl true
+    def handle_cast({:push, new_value}, initial_stack_values) do
+      ProgrammingElixir.Chapter18.Stash.update([new_value | initial_stack_values])
       {:noreply, [new_value | initial_stack_values]}
     end
 
--- test/programming_elixir/chapter18_test.exs
+++ test/programming_elixir/chapter18_test.exs
@@ -8,6 +8,7 @@ defmodule ProgrammingElixir.Chapter18Test do
   doctest Chapter18
 
   @stack ProgrammingElixir.Chapter18.Stack
+  @withStash ProgrammingElixir.Chapter18.StackWithStash
 
   test "supervisor should restart process after failure" do
     ProgrammingElixir.Chapter18.StackApp.start([1, 2, 3])
@@ -25,5 +26,51 @@ defmodule ProgrammingElixir.Chapter18Test do
     GenServer.cast(@stack, {:push, 2})
     assert GenServer.call(@stack, :pop) == 2
     assert GenServer.call(@stack, :pop) == 1
+  end
+
+  # we add a special keyword to the initial stack, when pop operation reach to that value it throws exception and after restart we have empty stack
+  test "supervisor should restart process after failure but cannot remember the last state" do
+    ProgrammingElixir.Chapter18.StackApp.start([])
+
+    GenServer.cast(@stack, {:push, 3})
+    GenServer.cast(@stack, {:push, "kill"})
+    GenServer.cast(@stack, {:push, 1})
+
+    assert GenServer.call(@stack, :pop) == 1
+
+    # we reach to 'kill' keyword
+    assert catch_exit(GenServer.call(@stack, :pop))
+
+    # just give sometime for supervisor to start the Stack process - it should be emtpy after restart
+    :timer.sleep(2000)
+
+    # pop from empty stack
+    assert catch_exit(GenServer.call(@stack, :pop))
+    :timer.sleep(2000)
+
+    GenServer.cast(@stack, {:push, 1})
+    GenServer.cast(@stack, {:push, 2})
+    assert GenServer.call(@stack, :pop) == 2
+    assert GenServer.call(@stack, :pop) == 1
+  end
+
+  test "supervisor should restart process after failure and remember last state before crash" do
+    ProgrammingElixir.Chapter18.StackAppWithStash.start([])
+
+    GenServer.cast(@withStash, {:push, 4})
+    GenServer.cast(@withStash, {:push, 3})
+    GenServer.cast(@withStash, {:push, "kill"})
+    GenServer.cast(@withStash, {:push, 1})
+
+    assert GenServer.call(@withStash, :pop) == 1
+
+    # we reach to 'kill' keyword and here process is going crash
+    assert catch_exit(GenServer.call(@withStash, :pop))
+
+    # just give sometime for supervisor to start the Stack process - it should contain whatever left after 'kill'
+    :timer.sleep(2000)
+    # pop from stack
+    assert GenServer.call(@withStash, :pop) == 3
+    assert GenServer.call(@withStash, :pop) == 4
   end
 end
