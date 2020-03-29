e10955db8b4c82d292f23c1d1d17d05e7dc388f7
navidhsz <nhassanzadeh@groupon.com>
2020-03-29 19:54:03 +0100

    add exercise 2 chapter 24

--- test/programming_elixir/chapter24_test.exs
+++ test/programming_elixir/chapter24_test.exs
@@ -24,4 +24,16 @@ defmodule ProgrammingElixir.Chapter24Test do
   test "Caesar cypher for Binary" do
     assert Chapter24.Caesar.encrypt(<<1, 2, 3>>, 1) == <<2, 3, 4>>
   end
+
+  # Exercise: Protocols-2
+
+  test "exercise 2 rot13(abc) = nop" do
+    path = Path.expand("data/british-proper-names.95", ".")
+    {:ok, data} = File.read(path)
+    data_list = data |> String.split("\n", trim: true)
+
+    assert data_list
+           |> Enum.map(&Chapter24.Caesar.rot13/1)
+           |> Enum.filter(&Enum.member?(data_list, &1)) == ["nop"]
+  end
 end
